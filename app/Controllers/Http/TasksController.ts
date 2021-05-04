be07d99a98dd39554da5916aafad80a07543d7e6
Diego Stiehl <seccomiro@users.noreply.github.com>
2021-05-04 17:28:05 -0300

    Add show action

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -4,14 +4,17 @@ import Task from 'App/Models/Task'
 export default class TasksController {
   public async index({ view }: HttpContextContract) {
     const tasks = await Task.all()
-    return view.render('tasks/index', { tasks })
+    return view.render('tasks.index', { tasks })
+  }
+
+  public async show({ params, view }: HttpContextContract) {
+    const task = await Task.find(params.id)
+    return view.render('tasks.show', { task })
   }
 
   public async create({}: HttpContextContract) {}
 
   public async store({}: HttpContextContract) {}
-
-  public async show({}: HttpContextContract) {}
 
   public async edit({}: HttpContextContract) {}
 
